f4cca9312c45e615cf2e2515344487688508212b
Robert Ableson <robertableson@gmail.com>
2022-06-03 20:36:19 -0400

    Shortcode base done.

---
+++ simple-shortcode.php
@@ -0,0 +1,10 @@
+<?php
+
+//This is a simple shortcode template for Wordpress.
+
+function ra_display_shortcode_template() {
+    $message = 'Hello world!'; 
+  
+    return $message;
+}
+add_shortcode('ra_display_shortcode_result', 'ra_display_shortcode_template');
